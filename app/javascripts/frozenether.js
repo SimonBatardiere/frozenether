253eeff6214f9d3fbf1c0146949c773d84042f5b
Simon Batardiere <simon.batardiere@gmail.com>
2017-04-30 19:57:21 +0200

    COntinue dev

--- app/javascripts/frozenether.js
+++ app/javascripts/frozenether.js
@@ -100,6 +100,7 @@ frozenether.Account = function(owner, amount, duration) {
 		self.html();
 		localStorage.setItem('first_name', 'accounts');
 		$('#popup_create').modal('toggle');
+		$('#create_button').prop('disabled', false);
 	}).catch(function() {
 		console.error('Create new Frozen Ether account failed');
 	});
@@ -139,16 +140,20 @@ frozenether.Account.prototype.update = function() {
 		frozen = instance;
 		return frozen.isExist.call(self.id, { from: self.owner });
 	}).then(function(exist) {
+		if (!exist) {
+			console.error('The account doesn\'t exist');
+			this.reject();
+			return;
+		}
 		return frozen.amount.call(self.id, { from: self.owner });
 	}).then(function(amount) {
 		self.amount = amount;
 		return frozen.remainingTime.call(self.id, { from: self.owner });
 	}).then(function(duration) {
 		self.duration = duration;
-	}).catch(function(e) {
+	}).catch(function() {
 		self.amount = undefined;
 		self.duration = undefined;
-		console.error(e);
 	});
 }
 
@@ -160,6 +165,13 @@ frozenether.Account.prototype.updateHtml = function() {
 		$(self.selector('summary_duration')).text(frozenether.durationToString(self.duration));
 		$(self.selector('section_amount')).text(frozenether.amountToString(self.amount));
 		$(self.selector('section_duration')).text(frozenether.durationToString(self.duration));
+		if (self.duration !== undefined && self.duration.gt(0)) {
+			$(self.selector('button_deposit')).prop('disabled', false);
+			$(self.selector('button_withdraw')).prop('disabled', true);
+		} else {
+			$(self.selector('button_deposit')).prop('disabled', true);
+			$(self.selector('button_withdraw')).prop('disabled', false);
+		}
 	});
 }
 
@@ -176,7 +188,7 @@ frozenether.Account.prototype.htmlSummary = function() {
 	$('#accounts_summary').append(html);
 
 	$(this.selector('summary')).on('click', function() {
-		frozenether.naviguate(self.identifier());
+		self.naviguate();
 	});
 }
 
@@ -190,11 +202,11 @@ frozenether.Account.prototype.htmlSection = function() {
 	html += '<div class="col-sm-3">';
 	html += '<img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid img-circle" alt="Account representation">';
 	html += '<div class="btn-group fe-account-controls" role="group" aria-label="Account\'s actions">';
-	html += '<button type="button" data-toggle="modal" href="#popup_deposit" class="btn btn-default" title="Deposit">';
+	html += '<button id="' + this.identifier('button_deposit') + '" type="button" data-toggle="modal" href="#popup_deposit" class="btn btn-default" title="Deposit">';
 	html += '<span class="glyphicon glyphicon-log-in" aria-hidden="true">';
 	html += '<span class="sr-only">Deposit</span>';
 	html += '</button>';
-	html += '<button type="button" data-toggle="modal" href="#popup_withdraw" class="btn btn-default" title="Withdraw">';
+	html += '<button id="' + this.identifier('button_withdraw') + '" type="button" data-toggle="modal" href="#popup_withdraw" class="btn btn-default" title="Withdraw">';
 	html += '<span class="glyphicon glyphicon-log-out" aria-hidden="true">';
 	html += '<span class="sr-only">Withdraw</span>';
 	html += '</button>';
@@ -238,8 +250,91 @@ frozenether.Account.prototype.html = function() {
 	this.updateHtml();
 }
 
+frozenether.Account.prototype.naviguate = function() {
+	this.updateHtml();
+	frozenether.naviguate(this.identifier());
+	$('#deposit_owner').text(this.owner);
+	$('#deposit_id').text(this.id);
+	$('#withdraw_owner').text(this.owner);
+	$('#withdraw_id').text(this.id);
+	$('#lengthen_owner').text(this.owner);
+	$('#lengthen_id').text(this.id);
+}
+
+frozenether.Account.prototype.deposit = function(amount) {
+	var self = this;
+	var frozen;
+
+	frozenether.contract.deployed().then(function(instance) {
+		frozen = instance;
+		return frozen.isExist.call(self.id, { from: self.owner });
+	}).then(function(exist) {
+		if (!exist) {
+			console.error('The account doesn\'t exist');
+			this.reject();
+			return;
+		}
+		return frozen.deposit(self.id, { from: self.owner, value: amount });
+	}).then(function() {
+		return self.updateHtml();
+	}).then(function() {
+		$('#popup_deposit').modal('toggle');
+		$('#deposit_button').prop('disabled', false);
+	}).catch(function() {
+		console.error('Deposit on Frozen Ether account failed');
+	});
+}
+
+frozenether.Account.prototype.withdraw = function(amount) {
+	var self = this;
+	var frozen;
+
+	frozenether.contract.deployed().then(function(instance) {
+		frozen = instance;
+		return frozen.isExist.call(self.id, { from: self.owner });
+	}).then(function(exist) {
+		if (!exist) {
+			console.error('The account doesn\'t exist');
+			this.reject();
+			return;
+		}
+		return frozen.withdraw(self.id, amount, { from: self.owner });
+	}).then(function() {
+		return self.updateHtml();
+	}).then(function() {
+		$('#popup_withdraw').modal('toggle');
+		$('#withdraw_button').prop('disabled', false);
+	}).catch(function() {
+		console.error('Withdraw from Frozen Ether account failed');
+	});
+}
+
+frozenether.Account.prototype.lengthen = function(duration) {
+	var self = this;
+	var frozen;
+
+	frozenether.contract.deployed().then(function(instance) {
+		frozen = instance;
+		return frozen.isExist.call(self.id, { from: self.owner });
+	}).then(function(exist) {
+		if (!exist) {
+			console.error('The account doesn\'t exist');
+			this.reject();
+			return;
+		}
+		return frozen.lengthen(self.id, duration, { from: self.owner });
+	}).then(function() {
+		return self.updateHtml();
+	}).then(function() {
+		$('#popup_lengthen').modal('toggle');
+		$('#lengthen_button').prop('disabled', false);
+	}).catch(function() {
+		console.error('Lengthen the Frozen Ether account failed');
+	});
+}
+
 frozenether.getAccount = function(owner, id) {
-	var found;
+	var found = undefined;
 
 	frozenether.accounts.forEach(function(account) {
 		if (account.isEqual(owner, id)) {
@@ -267,12 +362,61 @@ frozenether.create = function() {
 }
 
 frozenether.deposit = function() {
+	var owner = $('#deposit_owner').text();
+	var id = $('#deposit_id').text();
+	var amount;
+	var amount_value = parseFloat($('#deposit_amount_value').val());
+	var amount_unit = $('#deposit_amount_unit').text().trim();
+	var account;
+
+	amount = web3.toWei(amount_value, amount_unit);
+	account = frozenether.getAccount(owner, id);
+	if (account === undefined) {
+		console.error('Get account failed: owner(' + owner + ') id(' + id + ')');
+		return;
+	}
+
+	account.deposit(amount);
 }
 
 frozenether.withdraw = function() {
+	var owner = $('#withdraw_owner').text();
+	var id = $('#withdraw_id').text();
+	var amount;
+	var amount_value = parseFloat($('#withdraw_amount_value').val());
+	var amount_unit = $('#withdraw_amount_unit').text().trim();
+	var account;
+
+	if ($('#withdraw_all').is(':checked')) {
+		amount = web3.toWei(1000000000, 'Ether');
+	} else {
+		amount = web3.toWei(amount_value, amount_unit);
+		account = frozenether.getAccount(owner, id);
+	}
+	if (account === undefined) {
+		console.error('Get account failed: owner(' + owner + ') id(' + id + ')');
+		return;
+	}
+
+	account.withdraw(amount);
 }
 
 frozenether.lengthen = function() {
+	var owner = $('#lengthen_owner').text();
+	var id = $('#lengthen_id').text();
+	var duration;
+	var duration_value = parseInt($('#lengthen_duration_value').val());
+	var duration_unit = $('#lengthen_duration_unit').text().trim();
+	var account;
+
+	duration = frozenether.durationToInt(duration_value, duration_unit);
+	account = frozenether.getAccount(owner, id);
+	if (account === undefined) {
+		console.error('Get account failed: owner(' + owner + ') id(' + id + ')');
+		return;
+	}
+
+	account.lengthen(duration);
 }
 
 frozenether.onNewEthAccount = function(account) {
@@ -285,7 +429,6 @@ frozenether.naviguate = function(name) {
 	var nav;
 	var section;
 
-	console.log(name);
 	if (typeof frozenether.active.nav !== 'undefined') {
 		$(frozenether.active.nav).removeClass('active');
 		$(frozenether.active.nav + ' > a > span').text('');
@@ -307,8 +450,6 @@ frozenether.naviguate = function(name) {
 		section = '#section_accounts';
 	}
 
-	console.log(nav);
-	console.log(section);
 	$(nav).addClass('active');
 	$(nav + ' > a > span').text(' (current)');
 	$(section).show();
@@ -333,8 +474,7 @@ frozenether.initContract = function() {
 			} else if (accounts.length == 0) {
 				reject("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
 			}
-			frozenether.accounts = accounts;
-			frozenether.accounts.forEach(frozenether.onNewEthAccount);
+			accounts.forEach(frozenether.onNewEthAccount);
 			resolve();
 		});
 	});
@@ -388,17 +528,35 @@ frozenether.initDropdown = function() {
 	});
 }
 
+frozenether.initCheckbox = function() {
+	$('#withdraw_all').prop('checked', false);
+	$('#withdraw_all').change(function(event) {
+		if (event.target.checked) {
+			$('#withdraw_amount_value').val(0);
+			$('#withdraw_amount_value').prop('disabled', true);
+			$('#withdraw_amount_unit').prop('disabled', true);
+		} else {
+			$('#withdraw_amount_value').prop('disabled', false);
+			$('#withdraw_amount_unit').prop('disabled', false);
+		}
+	});
+}
+
 frozenether.initButton = function() {
 	$('#create_button').on('click', function() {
+		$('#create_button').prop('disabled', true);
 		frozenether.create();
 	});
 	$('#deposit_button').on('click', function() {
+		$('#deposit_button').prop('disabled', true);
 		frozenether.deposit();
 	});
 	$('#withdraw_button').on('click', function() {
+		$('#withdraw_button').prop('disabled', true);
 		frozenether.withdraw();
 	});
 	$('#lengthen_button').on('click', function() {
+		$('#lengthen_button').prop('disabled', true);
 		frozenether.lengthen();
 	});
 }
@@ -414,6 +572,8 @@ $(function() {
 		frozenether.initNav();
 	}).then(function() {
 		frozenether.initDropdown();
+	}).then(function() {
+		frozenether.initCheckbox();
 	}).then(function() {
 		frozenether.initButton();
 	}).catch(function(message) {
