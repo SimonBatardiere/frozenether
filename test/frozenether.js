e542f62d8f5ca96ed2cfd2b924d33c84dbc2b208
Simon Batardiere <simon.batardiere@gmail.com>
2017-05-01 18:51:24 +0200

    Update test contract

--- test/frozenether.js
+++ test/frozenether.js
@@ -1,10 +1,10 @@
-var FrozenEther = artifacts.require("./FrozenEther.sol");
+var FrozenEther = artifacts.require('./FrozenEther.sol');
 
 function increase_time(offset) {
 	return new Promise(function(resolve, reject) {
 		return web3.currentProvider.sendAsync({
-				jsonrpc: "2.0",
-				method: "evm_increaseTime",
+				jsonrpc: '2.0',
+				method: 'evm_increaseTime',
 				id: new Date().getTime(),
 				params: [ offset ]
 		}, error => {
@@ -19,7 +19,7 @@ function increase_time(offset) {
 
 function check(account, index, amount, duration, options)
 {
-	var prefix = "Account " + account + "[" + index + "] ";
+	var prefix = 'Account ' + account + '[' + index + '] ';
 	var contract;
 
 	return FrozenEther.deployed().then(function(instance) {
@@ -27,19 +27,19 @@ function check(account, index, amount, duration, options)
 		return contract.isExist(index, { from: account });
 	}).then(function(exist) {
 		if (options === undefined || options.exist === undefined || options.exist != 0) {
-			assert(exist, prefix + "doesn't exist");
+			assert(exist, prefix + 'doesn\'t exist');
 		}
 		return contract.amount(index, { from: account });
 	}).then(function(value) {
 		if (options === undefined || options.amount === undefined || options.amount != 0) {
-			assert(value.eq(amount), prefix + "has invalid amount(" + value.toString() + "/" +
-					amount.toString() + ")");
+			assert(value.eq(amount), prefix + 'has invalid amount(' + value.toString() + '/' +
+					amount.toString() + ')');
 		}
 		return contract.remainingTime(index, { from: account });
 	}).then(function(time) {
 		if (options === undefined || options.duration === undefined || options.duration != 0) {
-			assert(time.lte(duration), prefix + "has invalid remaining time(" + time.toString() + "/" +
-					duration.toString() + ")");
+			assert(time.lte(duration), prefix + 'has invalid remaining time(' + time.toString() + '/' +
+					duration.toString() + ')');
 		}
 	});
 }
@@ -92,27 +92,37 @@ function withdraw(account, index, amount, options) {
 	});
 }
 
+function lengthen(account, index, duration, options) {
+	var contract;
+	var total;
+
+	return FrozenEther.deployed().then(function(instance) {
+		contract = instance;
+		return contract.remainingTime(index, { from: account });
+	}).then(function(value) {
+		total = value.plus(duration);
+		return contract.lenghtenFrozenState(index, duration, { from: account });
+	}).then(function(tx) {
+		if (options === undefined) {
+			options = {};
+		}
+		options.amount = 0;
+		return check(account, index, 0, total, options);
+	});
+}
+
 contract('FrozenEther', function(accounts) {
 	var account;
 	var i;
 	var j;
 
-	it("Create one account", function() {
+	it('Create one account', function() {
 		return create(accounts[0], 1000, 1000, 1000);
-	});
-
-	it("Create a lot of accounts", function() {
-		for (i = 0; i < 10; i++) {
-			account = accounts[i];
-			for (j = 0; j < 10; j++) {
-				create(account, j, 15, 1000000);
-			}
-		}
 	});
 });
 
 contract('FrozenEther', function(accounts) {
-	it("Deposit Ether on an account", function() {
+	it('Deposit Ether on an account', function() {
 		return FrozenEther.deployed().then(function(instance) {
 			return create(accounts[0], 0, 50, 3);
 		}).then(function() {
@@ -120,7 +130,7 @@ contract('FrozenEther', function(accounts) {
 		});
 	});
 
-	it("Withdraw Ether from an account", function() {
+	it('Withdraw Ether from an account', function() {
 		return FrozenEther.deployed().then(function(instance) {
 			return create(accounts[0], 1, 50, 1000);
 		}).then(function() {
@@ -134,13 +144,25 @@ contract('FrozenEther', function(accounts) {
 		});
 	});
 
-	it("Withdraw more Ether than available from an account", function() {
+	it('Withdraw more Ether than available from an account', function() {
 		return FrozenEther.deployed().then(function(instance) {
 			return create(accounts[0], 1, 50, 1000);
 		}).then(function() {
 			return increase_time(1001);
 		}).then(function() {
 			return withdraw(accounts[0], 1, 60, { exist: 0 });
+		});
+	});
+
+	it('Lengthen the duration of an acount', function() {
+		return FrozenEther.deployed().then(function(instance) {
+			return create(accounts[0], 1, 50, 1000);
+		}).then(function() {
+			return lengthen(accounts[0], 1, 500, { duration: 0 });
+		}).then(function() {
+			return increase_time(2000);
+		}).then(function() {
+			return lengthen(accounts[0], 1, 500, { duration: 0 });
 		});
 	});
 });
